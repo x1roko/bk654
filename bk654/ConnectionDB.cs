0ab4b8d134afff42c8b5120ecec15ea1011a1966
x1roko <119897726+x1roko@users.noreply.github.com>
2024-06-06 02:46:27 +0300

    Добавьте файлы проекта.

---
+++ bk654/ConnectionDB.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace bk654
+{
+    public class ConnectionDB
+    {
+        private string connectionString;
+
+        public ConnectionDB(string server, string database, string uid, string password)
+        {
+            // Формирование строки подключения
+            connectionString = $"server={server};database={database};userid={uid};password={password}";
+        }
+
+        public MySqlConnection Connect()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+
+                connection.Open();
+
+                return connection;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public void Disconnect(MySqlConnection connection)
+        {
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Подключение закрыто.");
+            }
+        }
+    }
+}
---
+++ bk654/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace bk654
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MySqlConnection connection = new ConnectionDB("localhost", "bk654", "root", "").Connect();
+            using (connection)
+            {
+                //connection.Open();
+                /*string query = "INSERT INTO `bk654`.`user` (`name`, `surname`, `email`) VALUES ('anton', 'Nekrasov', 'asd@gmail.com');";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0); // Получаем значение ID
+                            string name = reader.GetString(1); // Получаем значение name
+
+                            Console.WriteLine($"ID: {id}, Name: {name}");
+                        }
+                    }
+                }*/
+            }
+        }
+    }
+}
